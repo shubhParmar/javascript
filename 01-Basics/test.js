c0a01f91fea0d1f0edc0759be24f4b4afe0fac30
shubhParmar <100068784+shubhParmar@users.noreply.github.com>
2024-05-31 09:37:31 +0000

    datatypes conversions done

--- 01-Basics/test.js
+++ 01-Basics/test.js
@@ -1,1 +1,37 @@
-console.log("shuhbham kumar");
+const accountId = 144553 // this cannot be reassigned or declaired again
+// accountId = 2 // not allowed
+
+let accountEmail = "hitesh@google.com" // let can be reassigned again but cannot be declaired again for a block scope
+
+var accountPassword = "12345" // var can be reassigned and declaired again for global scope
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+accountCity = "Jaipur" // a variable can be defined even without a datatype.
+
+let accountState; // stored a value "undifined" and allocated a memory space for it also known as hoisting
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]) 
+// for consoling more than one value.
+
+"use strict"; // to treat all js code as newer version
+
+//Datatypes types
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value , not 0
+// undefined => 
+// symbol => to provide uniqueness in variables 
+
+
+// object

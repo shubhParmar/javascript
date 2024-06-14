0d7d787ebabe0c639e9b4ba4dc0a451a9d5fa909
shubhParmar <100068784+shubhParmar@users.noreply.github.com>
2024-06-14 09:32:58 +0000

    new contents

---
+++ 01_Basics/test.js
@@ -0,0 +1,102 @@
+const accountId = 144553 // this cannot be reassigned or declaired again
+// accountId = 2 // not allowed
+
+let accountEmail = "hitesh@google.com" // let can be reassigned again but cannot be declaired again for a block scope
+
+var accountPassword = "12345" // var can be reassigned and declaired again for global scope
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+accountCity = "Jaipur" // a variable can be defined even without a datatype.
+
+let accountState; // stored a value "undifined" and allocated a memory space for it also known as hoisting
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+//console.log(accountId);
+
+//console.table([accountId, accountEmail, accountPassword, accountCity, accountState]) 
+// for consoling more than one value.
+
+"use strict"; // to treat all js code as newer version
+
+//**************************Datatypes types
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value , not 0
+// undefined => 
+// symbol => to provide uniqueness in variables 
+// object
+
+//let valueInNumber = Number(score) // type conversion of a number 
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN,  but will give datatype as number
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn) // type conversion into number
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber) //type conversion to string
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+ /*********************operations */
+
+// console.log("1" + 2);  =>12
+// console.log(1 + "2");  => 12
+// console.log("1" + 2 + 2);  =>122 operated by considering the first datatype which is string
+// console.log(1 + 2 + "2");  =>32 (first datatype is number)
+
+/*****************Comparisons */
+
+// console.log("2" > 1); => true
+// console.log("02" > 1); => true
+            // this is because the comparisons oprtors converts strings to number
+            
+// console.log(null > 0); => FALSE, as comparisons oprtors converts null to number(0)
+            // equality check(==) and comparisons (>,<,>=,<=) work differently
+
+// console.log(null == 0); =>FALSE, in equality, it is not converted as number(it is obect)
+
+// console.log(null >= 0); => true, converted to number (0)
+
+
+
+//**********************Summary */
+/*Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+            const id= '123';
+            const anotherId = '123';
+            console.log(id === anotherId) => false
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object*/
+
+       
--- 02_Basics/Functions.js
+++ 02_Basics/Functions.js
@@ -1,7 +1,7 @@
 function calCartPrice( val1, val2, ...val3){
     return (val1 , val2) // in return only last single value is returned 
 }
-console.log(calCartPrice(200,300,400,500));
+console.log(calCartPrice(200,300,400,500)); //=>300
 
 function addTwoNumbers(number1, number2){
 
@@ -33,7 +33,7 @@ function calculateCartPrice(val1, val2, ...num1){
 
  console.log(calculateCartPrice(200, 400, 500, 2000))
 
-const user = {
+const users = {
     username: "hitesh",
     prices: 199
 }
@@ -56,3 +56,122 @@ function returnSecondValue(getArray){
 
 // console.log(returnSecondValue(myNewArray));
 console.log(returnSecondValue([200, 400, 500, 1000]));
+
+//++++++++++++++++++SCOPE++++++++++++++++++++++++
+
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
+
+//++++++++++++++++++++++++ARROW Fntn+++++++++++++++++++++++
+
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);
+}
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwoo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwoo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
